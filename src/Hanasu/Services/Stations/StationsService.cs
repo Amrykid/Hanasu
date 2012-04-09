848e42ebb4f835ffea96d1bdadcd4d0fa0208d5f
Amrykid <amrykid@gmail.com>
2012-04-08 20:16:30 -0500

    Added 'streaming updates' of radio stations. Added two more stations.

--- src/Hanasu/Services/Stations/StationsService.cs
+++ src/Hanasu/Services/Stations/StationsService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Collections.ObjectModel;
 using Hanasu.Core;
 using System.Xml.Linq;
+using System.Timers;
 
 namespace Hanasu.Services.Stations
 {
@@ -21,13 +22,29 @@ namespace Hanasu.Services.Stations
         }
         public static StationsService Instance { get; private set; }
 
+        private Timer timer = null; //Timer is used for 'streaming updates' of new radio stations. That way, new  stations are added without reloading Hanasu.
+
         public StationsService()
         {
             Stations = new ObservableCollection<Station>();
+            timer = new Timer();
 
+            timer.Elapsed += timer_Elapsed;
+
+            timer.Interval = 20000; //20 seconds
+
+            LoadStationsFromRepo();
+
+            timer.Start();
+        }
+
+        private void LoadStationsFromRepo()
+        {
             try
             {
                 var doc = XDocument.Load("https://raw.github.com/Amrykid/Hanasu/master/src/Hanasu/Stations.xml");
+
+                Stations.Clear();
 
                 var stats = from x in doc.Element("Stations").Elements("Station")
                             select new Station()
@@ -45,6 +62,17 @@ namespace Hanasu.Services.Stations
             }
             catch (Exception) { }
             OnPropertyChanged("Stations");
+        }
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            int before = Stations.Count;
+
+            LoadStationsFromRepo();
+
+            if (Stations.Count > before)
+                Hanasu.Services.Notifications.NotificationsService.AddNotification("Stations Updated",
+                    (Stations.Count - before).ToString() + " station(s) added.", 4000);
         }
 
         public ObservableCollection<Station> Stations { get; private set; }
