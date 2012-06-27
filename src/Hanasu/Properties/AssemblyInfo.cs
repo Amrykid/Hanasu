a1f7764858eaee748191da8242e5e6a3caf294c1
Amrykid <amrykid@gmail.com>
2012-06-27 16:00:47 -0500

    updated assembly info / build version for some reason.

--- src/Hanasu/Properties/AssemblyInfo.cs
+++ src/Hanasu/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.566.0")]
-[assembly: AssemblyFileVersion("1.0.566.0")]
+[assembly: AssemblyVersion("1.0.567.0")]
+[assembly: AssemblyFileVersion("1.0.567.0")]
