64358e8afac0a96bfdd66f8a51910a66062330f0
Amrykid <amrykid@gmail.com>
2012-06-28 18:53:58 -0500

    Links in About window are now clickable.

--- src/Hanasu/Windows/AboutWindow.xaml.cs
+++ src/Hanasu/Windows/AboutWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Reflection;
 using MahApps.Metro.Controls;
+using System.Diagnostics;
 
 namespace Hanasu.Windows
 {
@@ -25,6 +26,21 @@ namespace Hanasu.Windows
             InitializeComponent();
 
             label2.Content = "Build Version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            this.Unloaded += new RoutedEventHandler(AboutWindow_Unloaded);
+        }
+
+        void AboutWindow_Unloaded(object sender, RoutedEventArgs e)
+        {
+            this.XAMPPLink.RequestNavigate -= new System.Windows.Navigation.RequestNavigateEventHandler(this.Hyperlink_RequestNavigate);
+            this.PhalanxiaLink.RequestNavigate -= new System.Windows.Navigation.RequestNavigateEventHandler(this.Hyperlink_RequestNavigate);
+            this.TobiLink.RequestNavigate -= new System.Windows.Navigation.RequestNavigateEventHandler(this.Hyperlink_RequestNavigate);
+            this.Unloaded -= new RoutedEventHandler(AboutWindow_Unloaded);
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
