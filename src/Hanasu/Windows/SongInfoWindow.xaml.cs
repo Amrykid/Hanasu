5c7bada24b2f2989a1819b25f560df21ef2615bc
Amrykid <amrykid@gmail.com>
2012-07-05 15:01:21 -0500

    You can now print/save lyrics from the Song Info window.

--- src/Hanasu/Windows/SongInfoWindow.xaml.cs
+++ src/Hanasu/Windows/SongInfoWindow.xaml.cs
@@ -28,6 +28,8 @@ namespace Hanasu.Windows
         }
         void SongInfoWindow_Unloaded(object sender, RoutedEventArgs e)
         {
+            SaveBtn.Click -= SaveBtn_Click;
+            PrintBtn.Click -= PrintBtn_Click;
             this.thisWindow.Loaded -= new System.Windows.RoutedEventHandler(this.thisWindow_Loaded);
             this.CloseBtn.Click -= new System.Windows.RoutedEventHandler(this.Close_Click);
             this.buyAlbumbtn.Click -= new System.Windows.RoutedEventHandler(this.buyAlbumbtn_Click);
@@ -49,8 +51,68 @@ namespace Hanasu.Windows
         {
             Hanasu.Services.Settings.SettingsThemeHelper.ApplyThemeAccordingToSettings(this);
 
-            if (((SongData)this.DataContext).BuyUri == null)
+            var songData = (SongData)this.DataContext;
+            if (songData.BuyUri == null)
                 buyAlbumbtn.IsEnabled = false;
+
+            if (songData.Lyrics == null)
+            {
+                SaveBtn.IsEnabled = false;
+                PrintBtn.IsEnabled = false;
+            }
+            else
+            {
+                SaveBtn.Click += SaveBtn_Click;
+                PrintBtn.Click += PrintBtn_Click;
+            }
+        }
+
+        void PrintBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var songData = (SongData)this.DataContext;
+
+            var diag = new PrintDialog();
+            diag.UserPageRangeEnabled = false;
+
+            if (diag.ShowDialog() == true)
+            {
+                var header = songData.Artist + " - " + songData.TrackTitle + " | Lyrics - Hanasu";
+
+                var doc = new FlowDocument();
+                doc.PagePadding = new Thickness(100);
+
+                doc.Blocks.Add(new Paragraph(new Run(header))
+                {
+                    TextAlignment = TextAlignment.Center,
+                    FontSize = 23
+                });
+
+                doc.Blocks.Add(new Paragraph(new Run(songData.Lyrics))
+                {
+                    TextAlignment = TextAlignment.Center
+                });
+
+                diag.PrintDocument(((IDocumentPaginatorSource)doc).DocumentPaginator, header);
+            }
+        }
+
+        void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Since Forms is already imported and the forms file dialog uses xp visuals, use it.
+            using (var sfd = new System.Windows.Forms.SaveFileDialog())
+            {
+                var songData = (SongData)this.DataContext;
+
+                sfd.Filter = "Text File (*.txt)|*.txt|All Files (*.*)|*.*";
+                sfd.FilterIndex = 1;
+                sfd.AutoUpgradeEnabled = true;
+                sfd.FileName = String.Format("{0}_{1}_lyrics.txt", songData.Artist, songData.TrackTitle);
+
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.Yes)
+                {
+                    System.IO.File.WriteAllText(sfd.FileName, songData.Lyrics);
+                }
+            }
         }
     }
 }
