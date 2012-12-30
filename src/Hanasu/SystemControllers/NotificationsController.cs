d221b1c30c51fa3eabed3e92da14828487ff976f
Amrykid <amrykid@gmail.com>
2012-12-29 19:02:36 -0800

    From last comment, started moving notifications stuff to it's own class.

---
+++ src/Hanasu/SystemControllers/NotificationsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+
+namespace Hanasu.SystemControllers
+{
+    public static class NotificationsController
+    {
+        public static void SendToast(string txt, string imgurl = "")
+        {
+            var toaster = ToastNotificationManager.CreateToastNotifier();
+
+            if (toaster.Setting != NotificationSetting.Enabled) return;
+
+            // It is possible to start from an existing template and modify what is needed.
+            // Alternatively you can construct the XML from scratch.
+            var toastXml = new Windows.Data.Xml.Dom.XmlDocument();
+            var title = toastXml.CreateElement("toast");
+            var visual = toastXml.CreateElement("visual");
+            visual.SetAttribute("version", "1");
+            visual.SetAttribute("lang", "en-US");
+
+            // The template is set to be a ToastImageAndText01. This tells the toast notification manager what to expect next.
+            var binding = toastXml.CreateElement("binding");
+            binding.SetAttribute("template", "ToastImageAndText01");
+
+            // An image element is then created under the ToastImageAndText01 XML node. The path to the image is specified
+            var image = toastXml.CreateElement("image");
+            image.SetAttribute("id", "1");
+            image.SetAttribute("src", imgurl);
+
+            // A text element is created under the ToastImageAndText01 XML node.
+            var text = toastXml.CreateElement("text");
+            text.SetAttribute("id", "1");
+            text.InnerText = txt;
+
+            // All the XML elements are chained up together.
+            title.AppendChild(visual);
+            visual.AppendChild(binding);
+            binding.AppendChild(image);
+            binding.AppendChild(text);
+
+            toastXml.AppendChild(title);
+
+            // Create a ToastNotification from our XML, and send it to the Toast Notification Manager
+            var toast = new ToastNotification(toastXml);
+            toaster.Show(toast);
+        }
+    }
+}
