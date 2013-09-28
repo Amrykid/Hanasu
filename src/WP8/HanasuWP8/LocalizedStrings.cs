bbe63257b98a2a36205568040fa133f4d4ed27f5
Amrykid <amrykid@gmail.com>
2013-09-28 09:20:21 -0700

    Initial testing of possible WP8 version of Hanasu.

---
+++ src/WP8/HanasuWP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HanasuWP8.Resources;
+
+namespace HanasuWP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ src/WP8/HanasuWP8/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using HanasuWP8.Resources;
+using Microsoft.Phone.BackgroundAudio;
+
+namespace HanasuWP8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void PhoneApplicationPage_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Phone.BackgroundAudio.BackgroundAudioPlayer.Instance.Track = new AudioTrack(){Source = new Uri("http://173.192.205.178:80"), PlayerControls = EnabledPlayerControls.Pause};
+            Microsoft.Phone.BackgroundAudio.BackgroundAudioPlayer.Instance.Play();
+            //AudioStreamingAgent asa = new AudioStreamingAgent();
+            //asa.
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
